2e7efd281a1e6b99f67cec72ad808d33763da76d
Chris Kay <chriskay25@gmail.com>
2019-07-16 18:46:47 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -14,9 +14,9 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT (characters.species).count
+  "SELECT characters.species, COUNT(characters.species)
   FROM characters
-  "
+  WHERE characters.species"
 end
 
 def select_name_and_series_subgenres_of_authors
