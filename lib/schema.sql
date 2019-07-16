59fbfa85e2b5e0c8871df301ca26db8673b09b63
Chris Kay <chriskay25@gmail.com>
2019-07-16 18:09:34 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -4,19 +4,22 @@ CREATE TABLE characters (
   
 CREATE TABLE series (
   id INTEGER PRIMARY KEY,
-  );
+  title TEXT,
+  author_id INTEGER);
   
 CREATE TABLE subgenre (
   id INTEGER PRIMARY KEY,
-  );
+  name TEXT);
   
 CREATE TABLE authors (
   id INTEGER PRIMARY KEY,
-  );
+  name TEXT);
   
 CREATE TABLE books (
   id INTEGER PRIMARY KEY,
-  );
+  title TEXT,
+  year INTEGER,
+  series_id INTEGER);
   
 CREATE TABLE character_books (
   id INTEGER PRIMARY KEY,
